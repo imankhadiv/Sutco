4f2f81a6a695615c9e44be48f4b27fa452f4c754
I Rastkhadivmasouleh <irastkhadivmasouleh1@sheffield.ac.uk>
2013-11-25 20:20:34 +0000

    drop down list added to FOH and Role and also some validation added to these models

--- app/models/foh.rb
+++ app/models/foh.rb
@@ -1,3 +1,6 @@
 class Foh < ActiveRecord::Base
   belongs_to :show
+  validates :available_number, :required_number, :position, :show, presence: true
+  validates :available_number, :required_number, :numericality => { :greater_than => 0}
+
 end
