4d47f93baa5d8dae403edfd914ab29ca44be669d
I Rastkhadivmasouleh <irastkhadivmasouleh1@sheffield.ac.uk>
2013-11-26 16:42:27 +0000

    schema changed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131125180016) do
+ActiveRecord::Schema.define(version: 20131125221909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,7 +85,7 @@ ActiveRecord::Schema.define(version: 20131125180016) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
+    t.integer  "applicant_id"
   end
 
   create_table "roles", force: true do |t|
