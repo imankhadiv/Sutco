b02bc1ebb4b4bab6306e27a3ac2eff7f89eadc5c
X Yang <XYang29@sheffield.ac.uk>
2013-11-27 12:14:26 +0000

    added show_management_spec.rb

---
+++ spec/factories/shows.rb
@@ -0,0 +1,15 @@
+# # == Schema Information
+# #
+# # Table name: shows
+# #
+# #
+
+
+ FactoryGirl.define do
+   factory :show do
+     name "Show 1"
+     director "Some Director"
+     producer "Some Producer"
+     synopsis "Some Description"
+   end
+ end
