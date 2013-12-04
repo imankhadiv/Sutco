8c1c6a10d5a63e5819878445c78090e0244fa0e7
I Rastkhadivmasouleh <irastkhadivmasouleh1@sheffield.ac.uk>
2013-12-04 11:12:59 +0000

    fix trainings error

---
+++ db/migrate/20131204110442_fix_trainings_table.rb
@@ -0,0 +1,16 @@
+class FixTrainingsTable < ActiveRecord::Migration
+
+  def change
+    drop_table :trainings
+    create_table :trainings do |t|
+      t.string :title
+      #type is a reserved word
+      #t.string :type
+      t.string :category
+      t.datetime :date
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131204002954) do
+ActiveRecord::Schema.define(version: 20131204110442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -138,7 +138,7 @@ ActiveRecord::Schema.define(version: 20131204002954) do
 
   create_table "trainings", force: true do |t|
     t.string   "title"
-    t.string   "type"
+    t.string   "category"
     t.datetime "date"
     t.text     "description"
     t.datetime "created_at"
