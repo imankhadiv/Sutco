b2b007abafa90e491a21c5fc364e278825c492db
I Rastkhadivmasouleh <irastkhadivmasouleh1@sheffield.ac.uk>
2013-12-05 22:33:06 +0000

    user validation added

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
@@ -90,7 +89,7 @@ ActiveRecord::Schema.define(version: 20131205115251) do
     t.datetime "updated_at"
   end
 
-create_table "users", force: true do |t|
+  create_table "users", force: true do |t|
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
@@ -113,7 +112,7 @@ create_table "users", force: true do |t|
 
   add_index "users", ["approved"], name: "index_users_on_approved", using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "workshop_attendances", force: true do |t|
     t.string   "username"
