085668a0cd8e32166d658f517d84eb4a616a0f75
X Yang <XYang29@sheffield.ac.uk>
2013-12-10 11:42:56 +0000

    changed hightlight in nav bar for sighup and login

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,10 +1,15 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_filter :set_nav_identifier
 
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << [:firstname, :lastname, :ucard, :course, :level]
   end
+
+    def set_nav_identifier
+	@current_nav_identifier	= :signup
+    end
 
  # def after_sign_up_path_for(resource)
  #   pages_awaiting_path
