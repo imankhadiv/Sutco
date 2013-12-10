020af117d03d1a069eeffcd500c094c0d2e27dff
X Yang <XYang29@sheffield.ac.uk>
2013-12-10 10:16:26 +0000

    added tests on calendar, changed high light on nav bar

--- spec/features/calendar_spec.rb
+++ spec/features/calendar_spec.rb
@@ -52,4 +52,66 @@ describe "Calendar tests" do
 		end		
 	end
 	
+	describe "Create events in calendar" do	
+		describe "Create a show in calendar" do	
+			 specify "I can create a show in calendar view" do
+visit calendars_path
+				  click_link "Create New Show"
+				  fill_in "Name", with: "Show 1"
+			    fill_in "Image", with: ""
+			    fill_in "Director", with: "Some Director"
+			    fill_in "Stage manager", with: "Some Stage manager"
+			    fill_in "Producer", with: "Some Producer"
+			    fill_in "Synopsis", with: "Some Description"
+			    click_button "Create Show"
+			    page.should have_content "Show was successfully created"
+			    page.should have_content "Show 1"
+			    page.should have_content "Some Director"
+			    page.should have_content "Some Stage manager"
+			    page.should have_content "Some Producer"
+			    page.should have_content "Some Description"
+			  end
+		end
+		
+		describe "Create a training session in calendar" do	
+			 specify "I can create a training session in calendar view" do
+			 	visit calendars_path
+				  click_link "Create New Training Session"
+			    fill_in "Title", with: "Training 1"
+			    fill_in "Category", with: "Lighting Operator"
+			    fill_in "Description", with: "Some description"
+			    select '2013', :from => 'Date'
+			    click_button "Create Training"
+			    page.should have_content "Training was successfully created"
+			    page.should have_content "Training 1"
+			  end
+		end
+		
+		describe "Create a workshop in calendar" do	
+			 specify "I can create a workshop in calendar view" do
+			 	visit calendars_path
+				  click_link "Create New Workshop"
+			    fill_in "Title", with: "Workshop 1"
+			    fill_in "Description", with: "Some description"
+			    #select_date(Date.tomorrow, from:"Date", datepicker: :bootstrap)
+			    click_button "Create Workshop"
+			    page.should have_content "Workshop was successfully created"
+			    page.should have_content "Workshop 1"
+			  end
+		end		
+		
+		describe "Create a social event in calendar" do	
+			 specify "I can create a social event in calendar view" do
+			 	visit calendars_path
+				  click_link "Create New Social Event"
+			    fill_in "Title", with: "Social 1"
+			    fill_in "Location", with: "RG-12"
+			    fill_in "Description", with: "Some description"
+			    select '2013', :from => 'Date'
+			    click_button "Create Social"
+			    page.should have_content "Social was successfully created"
+			    page.should have_content "Social 1"
+			  end
+		end
+  end	
 end
