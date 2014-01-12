b50bdb9021bda4461abceae3c19f1b40bd667aa0
S D Okunuga <soloistic1@gmail.com>
2014-01-12 15:32:26 +0000

    Validations

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,6 +1,8 @@
 class Show < ActiveRecord::Base
- has_many :show_dates, :dependent => :destroy
-accepts_nested_attributes_for :show_dates, :allow_destroy => true
- mount_uploader :image, ImageUploader
+  has_many :show_dates, :dependent => :destroy
+  accepts_nested_attributes_for :show_dates, :allow_destroy => true
+  mount_uploader :image, ImageUploader
+  validates :name, :director, :stage_manager, :producer, :synopsis, presence: true
+
 
 end
--- app/models/training.rb
+++ app/models/training.rb
@@ -1,2 +1,15 @@
 class Training < ActiveRecord::Base
+  validates :title, :category, :description, :date, :duration, :time, presence: true
+  validate :training_date_cannot_be_in_the_past
+  validate :training_time_cannot_be_in_the_past
+
+
+  def training_date_cannot_be_in_the_past
+    errors.add(:date, "can't be in the past") if
+        !date.blank? and date < Date.today
+  end
+  def training_time_cannot_be_in_the_past
+    errors.add(:time, "can't be in the past") if
+        date == Date.today and !time.blank? and time < Time.now
+  end
 end
