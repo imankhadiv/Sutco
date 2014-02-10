ef15eface8fc47281b1beeb822067c548d9c7573
I Rastkhadivmasouleh <irastkhadivmasouleh1@sheffield.ac.uk>
2014-02-10 13:38:38 +0000

    comments added to show, show_date and show_role

--- app/models/show_role.rb
+++ app/models/show_role.rb
@@ -1,3 +1,10 @@
+#
+#   show_role.rb
+#
+# This class belongs to Show. Each Show has different roles. The ShowRole class is implemented to provide information about different roles.
+#
+
+
 class ShowRole < ActiveRecord::Base
   belongs_to :show
   validates :available_number, :required_number, :name, :position, :show, presence: true
