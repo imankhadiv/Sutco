8a27d793485144fd52dde95e9e9239bf7a37b9d2
I Rastkhadivmasouleh <irastkhadivmasouleh1@sheffield.ac.uk>
2014-02-12 23:39:40 +0000

    training registeration improved

--- app/helpers/trainings_helper.rb
+++ app/helpers/trainings_helper.rb
@@ -1,2 +1,12 @@
 module TrainingsHelper
+
+  def check_training_attendee(training)
+    if(TrainingRecord.training_applied current_user.id, training.id).empty?
+       link_to "Attend Training", attend_training_path
+    else
+      flash[:notice] = "You have already registered for #{training.title}" unless flash[:notice]
+      flash.discard
+      nil
+    end
+  end
 end
