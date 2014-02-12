e1d70dcc53b80fb0e40c13e7fd575914c0b53919
I Rastkhadivmasouleh <irastkhadivmasouleh1@sheffield.ac.uk>
2014-02-12 14:05:01 +0000

    validation added to TrainingRecord

--- app/models/training_record.rb
+++ app/models/training_record.rb
@@ -1,10 +1,17 @@
 class TrainingRecord < ActiveRecord::Base
   belongs_to :user
   belongs_to :training
+  validates :training, uniqueness: {scope: :user,  message: "You can only apply for one position for a particular show date" }
+
 
   def self.create_new_record userID, trainingID
 
-    record = TrainingRecord.create(user_id: userID, training_id: trainingID)
+    record = TrainingRecord.new(user_id: userID, training_id: trainingID)
+  end
+
+  def self.registered_user
+
+
 
   end
 
