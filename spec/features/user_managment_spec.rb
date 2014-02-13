15c83516744c6dfb939e7ad22287c1302d7a1b67
X Yang <XYang29@sheffield.ac.uk>
2014-02-13 16:03:41 +0000

    added tests for user access control for show model

--- spec/features/user_managment_spec.rb
+++ spec/features/user_managment_spec.rb
@@ -49,7 +49,7 @@ describe "Access control for different roles" do
     user.roles << role
     login_as(user, :scope => :user)
     visit shows_path
-    page.should_not have_button "Create New Show"
+    page.should_not have_content "Create New Show"
     visit new_show_path
     page.should have_content "You are not authorized to access this page"
   end
@@ -60,7 +60,7 @@ describe "Access control for different roles" do
     user.roles << role
     login_as(user, :scope => :user)
     visit shows_path
-    page.should_not have_button "Create New Show"
+    page.should_not have_content "Create New Show"
     visit new_show_path
     page.should have_content "You are not authorized to access this page"
   end
@@ -71,7 +71,7 @@ describe "Access control for different roles" do
     user.roles << role
     login_as(user, :scope => :user)
     visit shows_path
-    page.should_not have_button "Create New Show"
+    page.should_not have_content "Create New Show"
     visit new_show_path
     page.should have_content "You are not authorized to access this page"
   end
@@ -82,13 +82,24 @@ describe "Access control for different roles" do
     user.roles << role
     login_as(user, :scope => :user)
     visit shows_path
-    page.should_not have_button "Create New Show"
+    page.should_not have_content "Create New Show"
     visit new_show_path
     page.should have_content "You are not authorized to access this page"
   end
 
   specify "As a senior committee, I can add shows" do
     role = Role.create :name =>"SeniorCommittee"
+    user = FactoryGirl.create(:user)
+    user.roles << role
+    login_as(user, :scope => :user)
+    visit shows_path
+    page.should have_content "Create New Show"
+    visit new_show_path
+    page.should_not have_content "You are not authorized to access this page"
+  end
+
+  specify "As a tech manager, I can add shows" do
+    role = Role.create :name =>"TechManager"
     user = FactoryGirl.create(:user)
     user.roles << role
     login_as(user, :scope => :user)
@@ -152,6 +163,32 @@ describe "Access control for different roles" do
     page.should have_content "You are not authorized to access this page"
   end
 
+ specify "As a senior committee, I can edit shows" do
+    role = Role.create :name =>"SeniorCommittee"
+    user = FactoryGirl.create(:user)
+    user.roles << role
+    login_as(user, :scope => :user)
+    visit shows_path
+    page.should have_link "Edit"
+    click_on "Show 1"
+    page.should have_link "Edit"
+    visit edit_show_path(show_with_show_dates.id)
+    page.should_not have_content "You are not authorized to access this page"
+  end
+
+ specify "As a tech manager, I can edit shows" do
+    role = Role.create :name =>"TechManager"
+    user = FactoryGirl.create(:user)
+    user.roles << role
+    login_as(user, :scope => :user)
+    visit shows_path
+    page.should have_link "Edit"
+    click_on "Show 1"
+    page.should have_link "Edit"
+    visit edit_show_path(show_with_show_dates.id)
+    page.should_not have_content "You are not authorized to access this page"
+  end
+
 #Deleting
   specify "As a member, I can't delete shows" do
     role = Role.create :name =>"Member"
@@ -197,7 +234,27 @@ describe "Access control for different roles" do
     page.should_not have_link "Delete"
   end
 
+ specify "As a senior committee, I can delete shows" do
+    role = Role.create :name =>"SeniorCommittee"
+    user = FactoryGirl.create(:user)
+    user.roles << role
+    login_as(user, :scope => :user)
+    visit shows_path
+    page.should have_link "Delete"
+    click_on "Show 1"
+    page.should have_link "Delete"
+  end
 
+ specify "As a tech manager, I can delete shows" do
+    role = Role.create :name =>"TechManager"
+    user = FactoryGirl.create(:user)
+    user.roles << role
+    login_as(user, :scope => :user)
+    visit shows_path
+    page.should have_link "Delete"
+    click_on "Show 1"
+    page.should have_link "Delete"
+  end
 
 end
 
