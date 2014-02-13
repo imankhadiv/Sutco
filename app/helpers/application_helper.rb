8ec5ed23cfcfc8766826f9ecc10894d21cb06e8f
S D Okunuga <soloistic1@gmail.com>
2014-02-13 21:34:05 +0000

    fix deprecation warning issues

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,6 +24,14 @@ module ApplicationHelper
     options )
   end
 
+  #improve later
+  def link_to_function(name, function, html_options={})
+    onclick = "#{"#{html_options[:onclick]}; " if html_options[:onclick]}#{function}; return false;"
+    href = html_options[:href] || '#'
+
+    content_tag(:a, name, html_options.merge(:href => href, :onclick => onclick))
+  end
+
   def link_to_remove_fields(name, f)
     f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)")
   end
--- app/helpers/shows_helper.rb
+++ app/helpers/shows_helper.rb
@@ -7,7 +7,7 @@ module ShowsHelper
         link_to_icon('icon-desktop','View', show),
         link_to_icon('icon-edit-sign','Edit', edit_show_path(show)),
         link_to_icon('icon-trash','Delete', show, {
-          confirm: 'Are you sure?',
+          data: {confirm: 'Are you sure?'},
           method: :delete
       })
       ].join(' ').html_safe
