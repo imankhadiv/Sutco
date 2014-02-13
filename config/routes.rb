1d6e11232df25d228091d830a8e8ef45850fef2d
S D Okunuga <soloistic1@gmail.com>
2014-02-13 20:01:48 +0000

    Navigation menu, dropdown, and routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,23 @@
 Team10MiniProject::Application.routes.draw do
 
-  resources :positions
-
-  resources :show_dates do
-    resources :fohs
-  end
+  root 'calendars#index'
+  get "pages/awaiting", as: :awaiting
+  get "pages/welcome", as: :welcome
 
   devise_for :users,  :controllers => { :registrations => "users/registrations" }
+  resources :users
+
+  resources :positions
+
+  resources :shows
+
+  resources :calendars
+
+  resources :show_dates do
+    resources :fohs, only: [:new, :create, :show]
+  end
+
+  resources :fohs, only: [:index]
 
   resources :trainings do
     member do
@@ -14,22 +25,13 @@ Team10MiniProject::Application.routes.draw do
       get :get_training_users
     end
   end
-
   resources :workshops
-
-  resources :users
-
-  #resources :fohs
 
   resources :show_roles
 
-  resources :shows
 
-  resources :calendars
 
   resources :socials
 
-  get "pages/awaiting"
-  get "pages/welcome"
-  root 'calendars#index'
+
 end
