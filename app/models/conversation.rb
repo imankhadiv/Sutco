851b84ab5f487022c3745ac6249f9dd9badccc18
I Rastkhadivmasouleh <iman.khadiv@gmail.com>
2014-03-26 10:00:34 +0000

    Added dependency to conversations

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,9 +1,10 @@
 class Conversation < ActiveRecord::Base
   has_many :comments, dependent: :destroy
+  has_many :notifications, dependent: :destroy
   belongs_to :user
   belongs_to :board
   validates :title, :body, presence: true
-  default_scope order('updated_at DESC')
+  default_scope order ('updated_at DESC')
 
 
 end
