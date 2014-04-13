388f248b2109a2b4b9732d2a629f82d126f69c53
S D Okunuga <soloistic1@gmail.com>
2014-04-13 19:12:01 +0100

    began addition of more comments and confirming that jenkins coverage now displays

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,13 @@
 class PagesController < ApplicationController
   #before_filter :authenticate_user!
 
+  #Home page
   def welcome
     authenticate_user!
     @current_nav_identifier = :home
   end
 
+  #page for after sign up but not yet activated
   def awaiting
 
   end
