aa63cd11744040373a0e4478c2528f9ada428e60
X Yang <XYang29@sheffield.ac.uk>
2014-05-12 13:03:53 +0100

    changed css for social and workshop

--- app/helpers/socials_helper.rb
+++ app/helpers/socials_helper.rb
@@ -4,6 +4,26 @@
 # This is an helper class for Socials. It contains a method for displaying navigation links based on the current logged in user
 #
 module SocialsHelper
+
+# This method is implemented to display the social name and three different icons for view, edit and delete buttons.
+  def social_name_links(social)
+    content_tag :h3 do
+    if ((current_user.role? "SeniorCommittee") || (current_user.role? "TechManager")|| (current_user.role? "ProductionTeam"))
+      [
+        link_to_button('buttonshowname',social.title,social),
+        link_to_button('buttonedit','Edit', edit_social_path(social)),
+        link_to_button('buttondelete','Delete', social, {
+          data: {confirm: 'Are you sure?'},
+          method: :delete
+      })
+      ].join(' ').html_safe
+    else
+      [ link_to(social.title,social)
+      ].join(' ').html_safe
+    end
+    end
+  end
+
 # This method controls the navigation links for different user roles
 def social_navigation_links(social)
     content_tag :h3 do
--- app/helpers/workshops_helper.rb
+++ app/helpers/workshops_helper.rb
@@ -6,6 +6,26 @@
 # It also provides custom display methods for workshops and can check a current user's attendance for an event
 #
 module WorkshopsHelper
+
+# This method is implemented to display the workshop name and three different icons for view, edit and delete buttons.
+  def workshop_name_links(workshop)
+    content_tag :h3 do
+    if ((current_user.role? "SeniorCommittee") || (current_user.role? "TechManager"))
+      [
+        link_to_button('buttonshowname',workshop.title,workshop),
+        link_to_button('buttonedit','Edit', edit_workshop_path(workshop)),
+        link_to_button('buttondelete','Delete', workshop, {
+          data: {confirm: 'Are you sure?'},
+          method: :delete
+      })
+      ].join(' ').html_safe
+    else
+      [ link_to(workshop.title,workshop)
+      ].join(' ').html_safe
+    end
+    end
+  end
+
   # This method checks if a user can attend or has registered to attend a training
   def check_workshop_attendee(workshop)
     if(WorkshopRecord.workshop_applied current_user.id, workshop.id).empty?
